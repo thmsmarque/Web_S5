f57a525077c84e9e76bec6a873ed791c780d2c46
SeraBralah <serabralah@gmail.com>
2024-11-14 14:58:25 +0100

    Ajout des données + index

---
+++ app/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Hello Page</title>
+</head>
+
+<body>
+    <?php
+    echo "Hello.... Beautiful";
+    include '../ressources/Donnees.inc.php';
+
+    //print_r($Recettes);
+    ?>
+</body>
+
+</html>
---
+++ ressources/install_bdd.php
@@ -0,0 +1,18 @@
+<?php
+$hostname = "sql308.byethost16.com";
+$username = "b16_37711947";
+$password = "";
+$db_name = "b16_37711947_drinkdrink";
+
+echo "test";
+
+$conn = mysqli_connect($hostname, $username, $password, $db_name);
+
+if (!$conn) {
+    die("Erreur de connexion : " . mysqli_connect_error());
+}
+
+echo "Connection successful";
+
+mysqli_close($conn);
+
