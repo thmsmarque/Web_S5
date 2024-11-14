f4992d271e825eb9d964562ac63e3f055adc0f66
SeraBralah <serabralah@gmail.com>
2024-11-14 15:47:08 +0100

    Premier affichage

--- app/index.php
+++ app/index.php
@@ -2,16 +2,39 @@
 <html>
 
 <head>
-    <title>Hello Page</title>
+    <title>Index</title>
+    <link rel="stylesheet" href="style.css" />
 </head>
 
 <body>
-    <?php
-    echo "Hello.... Beautiful";
-    include '../ressources/Donnees.inc.php';
 
-    //print_r($Recettes);
-    ?>
+    <header class="bandeau">
+        <div style="display: flex; justify-content: space-between; align-items: center;">
+            <h1>
+                Drink-drink !
+            </h1>
+            <nav>
+                <form>
+                    <input type="search" name="q" placeholder="La boisson de vos rêves..." />
+                    <input type="submit" value="Go" />
+                </form>
+            </nav>
+        </div>
+    </header>
+    <main> <!-- Contenu de la page -->
+        <article class="recette_contenu">
+            <article class="titre_recette">
+                <?php
+                include '../ressources/Donnees.inc.php';
+                /* Récupérer les titres : */
+                foreach ($Recettes as $recette) {
+                    echo $recette['titre'];
+                    echo "<br>";
+                }
+                ?>
+            </article>
+        </article>
+    </main>
 </body>
 
 </html>
