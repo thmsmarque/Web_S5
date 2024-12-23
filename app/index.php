cb3c30ce40f2605e5a869c82a92a78b0c23c6781
Abrlh <serabralah@gmail.com>
2024-12-23 01:36:34 +0100

    Début barre latérale

--- app/index.php
+++ app/index.php
@@ -35,44 +35,57 @@
 </head>
 
 <body>
-    <header class="bandeau">
-        <div style="display: flex; justify-content: space-between; align-items: center;" class ="upper-bandeau">
-            <h1>
-                Drink-drink !
-            </h1>
-            <nav>
-                <form>
-                    <input type="search" name="q" placeholder="La boisson de vos rêves..." />
-                    <input type="submit" value="Go" />
-                </form>
-            </nav>
+    <div class="sidebar">
+            <h2>Ingrédients</h2>
+            <ul>
+               test
+            <br>
+               test 3
+               <br>
+               test 4
+               <br>
+        </ul>
+    </div>
+    <div class="content">
+        <header class="bandeau">
+            <div style="display: flex; justify-content: space-between; align-items: center;" class ="upper-bandeau">
+                <h1>
+                    Drink-drink !
+                </h1>
+                <nav>
+                    <form>
+                        <input type="search" name="q" placeholder="La boisson de vos rêves..." />
+                        <input type="submit" value="Go" />
+                    </form>
+                </nav>
 
-            <div class="login-buttons">
-                <div class="errorDisplay">
-                    <?php
-                    if (isset($_SESSION['errorConnexion'])) {
-                        echo $_SESSION['errorConnexion'];
-                        unset($_SESSION['errorConnexion']);
-                    }
-                    ?>
-                <form action="form_back/connexion_form.php" method="post">
-                    <?php
-                    #echo($_SESSION['isRegistered']);
-                    #echo(isset($_SESSION['isRegistered']));
-                    #echo($_SESSION["activeUser"]);
-                    if (isset($_SESSION['activeUser'])) {
-                        #echo '<button type="button" onclick="deconnexion()">Déconnexion</button>';
-                        echo'<a href="form_back/profil.php" class="deconnexion">Profil</a>';
-                    } else {
-                        echo '<div class="inputfields-for-login"><input type="text" name="username" id="username" placeholder="Nom d\'utilisateur"><input type="password" name="password" id="password" placeholder="Mot de passe">';
-                        echo '<button type="submit"}">Connexion</button>';
-                        echo '<button type="button" onclick="window.location.href=\'inscription.php\'">Inscription</button></div>';
-                        
-                    }
-                    ?>
-                </form>
-            </div>
+                <div class="login-buttons">
+                    <div class="errorDisplay">
+                        <?php
+                        if (isset($_SESSION['errorConnexion'])) {
+                            echo $_SESSION['errorConnexion'];
+                            unset($_SESSION['errorConnexion']);
+                        }
+                        ?>
+                    <form action="form_back/connexion_form.php" method="post">
+                        <?php
+                        #echo($_SESSION['isRegistered']);
+                        #echo(isset($_SESSION['isRegistered']));
+                        #echo($_SESSION["activeUser"]);
+                        if (isset($_SESSION['activeUser'])) {
+                            #echo '<button type="button" onclick="deconnexion()">Déconnexion</button>';
+                            echo'<a href="form_back/profil.php" class="deconnexion">Profil</a>';
+                        } else {
+                            echo '<div class="inputfields-for-login"><input type="text" name="username" id="username" placeholder="Nom d\'utilisateur"><input type="password" name="password" id="password" placeholder="Mot de passe">';
+                            echo '<button type="submit"}">Connexion</button>';
+                            echo '<button type="button" onclick="window.location.href=\'inscription.php\'">Inscription</button></div>';
+                            
+                        }
+                        ?>
+                    </form>
+                </div>
         </div>
+    </div>
         <div class="lower-bandeau">
 
         </div>
